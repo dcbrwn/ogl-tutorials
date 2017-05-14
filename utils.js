045ac83a26601da33a44575ad25b58b8b562ceea
Danila Shutov <dcbrwn2@gmail.com>
2017-05-14 13:40:03 +0300

    Add an init function

--- utils.js
+++ utils.js
@@ -1,3 +1,18 @@
 'use strict';
 
 window.canvas = document.getElementById('canvas');
+
+function initGL() {
+  window.gl = canvas.getContext("webgl");
+
+  if (!gl) {
+    throw 'WebGL is not supported';
+  }
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
