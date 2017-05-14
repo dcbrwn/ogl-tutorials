09490d5ea3db80277ab54f3beaad31817d1ecaef
Danila Shutov <dcbrwn2@gmail.com>
2017-05-14 14:54:56 +0300

    Add a function to load GL programs
    Implement basic tutorial #2

--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 'use strict';
 
-window.canvas = document.getElementById('canvas');
 
 function initGL() {
-  window.gl = canvas.getContext("webgl");
+  const canvas = document.getElementById('canvas');
+  const gl = canvas.getContext("webgl");
 
   if (!gl) {
     throw 'WebGL is not supported';
@@ -15,4 +15,66 @@ function initGL() {
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  return gl;
+}
+
+function setRenderFunc(func) {
+  let prevTime = Date.now();
+
+  function render() {
+    let currentTime = Date.now();
+    const deltaTime = currentTime - prevTime;
+    prevTime = currentTime;
+
+    func(deltaTime);
+    requestAnimationFrame(render);
+  }
+
+  render();
+}
+
+function loadProgram(gl, vertexShaderUrl, fragmentShaderUrl) {
+  const program = gl.createProgram();
+  let vertexShader;
+  let fragmentShader;
+
+  return Promise
+    .all([
+      fetch(vertexShaderUrl).then((res) => res.text()),
+      fetch(fragmentShaderUrl).then((res) => res.text()),
+    ])
+    .then((shaders) => {
+      vertexShader = gl.createShader(gl.VERTEX_SHADER);
+      gl.shaderSource(vertexShader, shaders[0]);
+      gl.compileShader(vertexShader);
+
+      if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        throw 'Could not compile vertex shader:\n' + gl.getShaderInfoLog(vertexShader);
+      }
+
+      fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+      gl.shaderSource(fragmentShader, shaders[1]);
+      gl.compileShader(fragmentShader);
+
+      if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        throw 'Could not compile vertex shader:\n' + gl.getShaderInfoLog(fragmentShader);
+      }
+
+      gl.attachShader(program, vertexShader);
+      gl.attachShader(program, fragmentShader);
+      gl.linkProgram(program);
+
+      if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        throw 'Could not compile WebGL program:\n' + gl.getProgramInfoLog(program);
+      }
+
+      return program;
+    })
+    .catch((error) => {
+      gl.deleteProgram(program);
+      gl.deleteShader(vertexShader);
+      gl.deleteShader(fragmentShader);
+      throw error;
+    });
 }
