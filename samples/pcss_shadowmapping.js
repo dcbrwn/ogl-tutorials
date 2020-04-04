7cb46cb6034d04370ac56af8c7580b90d5bfbbe6
Danila Shutov <dcbrwn2@gmail.com>
2020-04-05 00:12:37 +0300

    PCSS-ish shadows

--- samples/pcss_shadowmapping.js
+++ samples/pcss_shadowmapping.js
@@ -1,14 +1,14 @@
 import { initGL, loadProgram, loadObj, setRenderFunc, glsl } from "../lib/utils.js";
 import { PerspectiveCamera } from "../lib/PerspectiveCamera.js";
 import { calcFlatNormalsForVertices } from "../lib/math.js";
-import { DepthFramebuffer, Framebuffer } from "../lib/Framebuffer.js";
+import { DepthFramebuffer } from "../lib/Framebuffer.js";
 import * as vec3 from "../vendor/gl-matrix/vec3.js";
 import { toRadian } from "../vendor/gl-matrix/common.js";
 import { Material } from "../lib/Material.js";
 
 const gl = initGL();
 
-const floorHeight = 0;
+const floorHeight = 0.3;
 
 const sceneVertices = new Float32Array([
   // FLOOR
@@ -30,7 +30,12 @@ const normalsBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, normalsBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, sceneNormals.buffer, gl.STATIC_DRAW);
 
-const lightCamera = new PerspectiveCamera(toRadian(90), 1, 1.0, 50);
+const lightCamera = new PerspectiveCamera(toRadian(90), 1, 1.0, 100);
+lightCamera.lookAt(
+  vec3.fromValues(-2, 3, 2),
+  vec3.fromValues(0, 0, 0),
+  vec3.fromValues(0, 0, 1)
+);
 
 const camera = new PerspectiveCamera(toRadian(30), 4 / 3, 0.1, 50);
 camera.lookAt(
@@ -41,7 +46,7 @@ camera.lookAt(
 
 async function main() {
   const shadowMapRez = 512;
-  const depthFramebuffer = new Framebuffer(gl, shadowMapRez, shadowMapRez);
+  const depthFramebuffer = new DepthFramebuffer(gl, shadowMapRez, shadowMapRez);
 
   const lightMaterial = new Material(gl, {
     attributes: ["aPosition"],
@@ -54,13 +59,10 @@ async function main() {
 
       in vec3 aPosition;
 
-      out vec3 position;
-
       uniform mat4 transformMatrix;
 
       void main() {
           gl_Position = transformMatrix * vec4(aPosition, 1.0);
-          position = gl_Position.xyz;
       }
     `,
     fragmentShader: glsl`
@@ -69,27 +71,20 @@ async function main() {
       precision highp float;
       precision highp int;
 
-      in vec3 position;
-      out vec4 color;
+      in float depth;
 
       void main() {
-        float moment1 = clamp(length(position.xyz) / 50.0, 0.0, 1.0);
-        float moment2 = moment1 * moment1;
-
-        float dx = dFdx(moment1);
-        float dy = dFdy(moment1);
-        moment2 += 0.25*(dx*dx+dy*dy);
-
-        color = vec4(moment1, moment2, 0.0, 1.0);
+        gl_FragDepth = gl_FragCoord.z;
       }
     `,
-  });
+  })
+
+  const lightProgram = lightMaterial.getProgram();
 
   const sceneMaterial = new Material(gl, {
     attributes: ["aPosition", "aNormal"],
     uniforms: [
       "transformMatrix",
-      "uLightPos",
       "uLightMatrix",
       "uShadowMap",
     ],
@@ -102,7 +97,6 @@ async function main() {
       in vec3 aPosition;
       in vec3 aNormal;
 
-      uniform vec3 uLightPos;
       uniform mat4 uLightMatrix;
       uniform mat4 transformMatrix;
 
@@ -113,11 +107,12 @@ async function main() {
       out vec4 fragPosLightSpace;
 
       void main() {
+        const vec3 lightPos = vec3(-2.0, 3.0, 2.0);
         const vec3 lightTarget = vec3(0.0, 0.0, 0.0);
         const vec3 cameraPos = vec3(4, 3, 3);
 
-        toLight = uLightPos - aPosition;
-        lightDir = normalize(lightTarget - uLightPos);
+        toLight = lightPos - aPosition;
+        lightDir = normalize(lightTarget - lightPos);
         toCamera = cameraPos - aPosition;
         normal.xyz = aNormal;
         fragPosLightSpace = uLightMatrix * vec4(aPosition, 1.0);
@@ -144,22 +139,81 @@ async function main() {
         return clamp((v - low) / (high - low), 0.0, 1.0);
       }
 
-      float VSM(sampler2D uShadowMap, vec2 uv, float compare) {
-        vec2 moments = texture(uShadowMap, uv).xy;
-        float p = smoothstep(compare - 0.001, compare, moments.x);
-        float variance = max(moments.y - moments.x*moments.x, -0.1);
-        float d = compare - moments.x;
-        float p_max = linstep(0.2, 1.0, variance / (variance + d*d));
-        return clamp(max(p, p_max), 0.0, 1.0);
+      const float PI = 3.14159265f;
+      const float GoldenAngle = PI * (3.0 - sqrt(5.0));
+
+      vec2 vogel(uint sampleIndex, uint samplesCount, float offset) {
+        float r = sqrt(float(sampleIndex) + 0.5f) / sqrt(float(samplesCount));
+        float theta = float(sampleIndex) * GoldenAngle + offset;
+        return r * vec2(cos(theta), sin(theta));
+      }
+
+      float getOccludersDepth(sampler2D uShadowMap, vec2 center, float currentDepth, float spread) {
+        float average = 0.0;
+        float counter = 0.0;
+
+        for (uint i = 0u; i < 20u; i++) {
+          vec2 offset = vogel(i, 20u, 0.0) * spread;
+          float sampledDepth = texture(uShadowMap, center + offset).x;
+
+          if (sampledDepth < currentDepth) {
+            average += sampledDepth;
+            counter += 1.0;
+          }
+        }
+
+        return average / counter;
+      }
+
+      float getPenumbra(sampler2D uShadowMap, vec2 center, float currentDepth, float spread) {
+        float bias = 0.005;
+        float average = 0.0;
+
+        for (uint i = 0u; i < 50u; i++) {
+          vec2 offset = vogel(i, 50u, 0.0) * spread;
+          float closestDepth = texture(uShadowMap, center + offset).x;
+
+          if (currentDepth - bias > closestDepth) {
+            average += closestDepth * ((2.0 * spread) / length(offset - center));
+          }
+        }
+
+        return average / 20.0;
       }
 
       float calcShadow(vec4 fragPosLightSpace) {
+        // perform perspective divide
         vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
         projCoords = projCoords * 0.5 + 0.5;
 
-        float currentDepth = clamp(length(fragPosLightSpace.xyz) / 50.0, 0.0, 1.0);
+        float currentDepth = projCoords.z;
+        float bias = 0.05;
 
-        return VSM(uShadowMap, projCoords.xy, currentDepth);
+
+        float closestDepth = texture(
+          uShadowMap,
+          projCoords.xy
+        ).r;
+
+        float occluderDepth = getOccludersDepth(
+          uShadowMap,
+          projCoords.xy,
+          currentDepth + bias,
+          0.01
+        );
+
+        float distance = 10.0 * (currentDepth - occluderDepth) / occluderDepth;
+
+        if (distance < bias) {
+          return 0.0;
+        }
+
+        return getPenumbra(
+          uShadowMap,
+          projCoords.xy,
+          currentDepth,
+          distance
+        );
       }
 
       void main() {
@@ -178,7 +232,7 @@ async function main() {
         float cosine = dot(l, -lightDir);
         float spot = cosine > 0.7 ? pow(cosine, 15.0) : 0.0;
 
-        float shadow = calcShadow(fragPosLightSpace);
+        float shadow = 1.0 - calcShadow(fragPosLightSpace);
         fragColor = vec4(ambColor + (diff + spec) * max(0.05, shadow) * spot, 1.0);
         // fragColor.rgb = vec3(shadow);
         // fragColor.a = 1.0;
@@ -188,9 +242,6 @@ async function main() {
 
   const mesh = await loadObj(gl, 'assets/teapot.obj');
 
-  const lightPos = vec3.fromValues(-2, 3, 2);
-  const origin = vec3.fromValues(0, 0, 0);
-
   setRenderFunc((dt) => {
     // Setup
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -199,44 +250,29 @@ async function main() {
     gl.enable(gl.CULL_FACE);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    vec3.rotateY(lightPos, lightPos, origin, toRadian(dt / 10));
-
     // Render shadow map
     {
-      depthFramebuffer.bind();
-      gl.viewport(0, 0, shadowMapRez, shadowMapRez);
-
-      lightCamera.lookAt(
-        lightPos,
-        vec3.fromValues(0, 0, 0),
-        vec3.fromValues(0, 0, 1)
-      );
-
       gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
       gl.enableVertexAttribArray(lightMaterial.attr.aPosition);
       gl.vertexAttribPointer(lightMaterial.attr.aPosition, 3, gl.FLOAT, false, 0, 0);
+
+      depthFramebuffer.bind();
+
+      gl.viewport(0, 0, shadowMapRez, shadowMapRez);
 
       lightMaterial.use();
 
       gl.uniformMatrix4fv(lightMaterial.uniforms.transformMatrix, false, lightCamera.transformMatrix);
 
-      gl.cullFace(gl.BACK);
-
       gl.drawArrays(gl.TRIANGLES, 0, 6);
 
       gl.disableVertexAttribArray(lightMaterial.attr.aPosition);
 
-      mesh.render(lightMaterial.getProgram(), lightCamera);
-
-      gl.bindTexture(gl.TEXTURE_2D, depthFramebuffer.targetTexture);
-      gl.generateMipmap(gl.TEXTURE_2D);
+      mesh.render(lightProgram, lightCamera);
     }
 
     // Render scene
     {
-      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-      gl.viewport(0, 0, 800, 600);
-
       gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
       gl.enableVertexAttribArray(sceneMaterial.attr.aPosition);
       gl.vertexAttribPointer(sceneMaterial.attr.aPosition, 3, gl.FLOAT, false, 0, 0);
@@ -245,14 +281,14 @@ async function main() {
       gl.enableVertexAttribArray(sceneMaterial.attr.aNormal);
       gl.vertexAttribPointer(sceneMaterial.attr.aNormal, 3, gl.FLOAT, false, 0, 0);
 
+      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+      gl.viewport(0, 0, 800, 600);
+
       sceneMaterial.use();
 
-      gl.uniform3fv(sceneMaterial.uniforms.uLightPos, lightPos);
       gl.uniformMatrix4fv(sceneMaterial.uniforms.transformMatrixUniform, false, camera.transformMatrix);
       gl.uniformMatrix4fv(sceneMaterial.uniforms.uLightMatrix, false, lightCamera.transformMatrix);
       gl.uniform1i(sceneMaterial.uniforms.uShadowMap, /** @type {number} */(depthFramebuffer.targetTexture));
-
-      gl.cullFace(gl.BACK);
 
       gl.drawArrays(gl.TRIANGLES, 0, 6);
       gl.disableVertexAttribArray(sceneMaterial.attr.aPosition);
